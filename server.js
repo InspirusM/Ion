bdb5868959da971887690e255a71d6c523447e9c
Glitch (musicbeast) <none>
2018-10-20 08:18:53 +0000

    🎢😜 Updated with Glitch

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  http.get(`http://${process.env.PROJECT_DOMAIN}/`);
 }, 280000);
 
 const Discord = require('discord.js')
